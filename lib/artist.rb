e64b21044833b2285c49467a1d6a23c61d6eb8b0
Angela <angelalamluu@gmail.com>
2020-12-06 21:58:47 -0800

    commit tree a85b0ca6786df2e7d75485cb0b3551a1df627c6f

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,33 @@
+class Artist
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.map {|song| song.genre}
+    end
+
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,32 @@
+class Patient
+    attr_accessor :name, :doctor, :appointment
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self}
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def doctors
+       appointments.collect {|appointment| appointment.doctor} 
+    end
+
+
+end
